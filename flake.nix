0373b94b1d67144b2e5073e56468e470c758c17b
cofob <cofob@riseup.net>
2024-02-18 22:08:28 +0700

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,30 +2,36 @@
   description = "Stateless captcha telegram bot";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    let
+      all = flake-utils.lib.eachDefaultSystem (system:
         let
-          pkgs = import nixpkgs { inherit system; };
-          env = pkgs.poetry2nix.mkPoetryEnv {
-            projectDir = ./.;
-            editablePackageSources = {
-              app = ./src;
-            };
+          pkgs = nixpkgs.legacyPackages.${system}.extend poetry2nix.overlays.default;
+          tg-captcha = pkgs.callPackage ./package.nix { };
+        in {
+          packages = {
+            inherit tg-captcha;
+            default = tg-captcha;
           };
-        in
-        {
-          devShells.default = env.env.overrideAttrs (oldAttrs: {
-            buildInputs = with pkgs; [ python3 poetry python3Packages.jedi-language-server ];
-          });
 
-          packages.default = pkgs.callPackage ./package.nix { };
-        }
-      ) // {
-      nixosModule = import ./module.nix;
-    };
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ tg-captcha ];
+            packages = [ pkgs.poetry ];
+          };
+        });
+    in {
+      nixosModules.tg-captcha = import ./module.nix;
+      nixosModules.default = self.nixosModules.tg-captcha;
+
+      overlays.tg-captcha = (final: prev: { tg-captcha = all.packages.${prev.system}.default; });
+      overlays.default = self.overlays.tg-captcha;
+    } // all;
 }
